005d6610ff7b6ebeebb20d2c0e226719e7d5eaf9
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-06-12 13:19:23 -0700

    updating the model and attaching the shader material

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -7,38 +7,66 @@ import {
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
+import jellyVertexShader from "./shaders/hologram/vertex.glsl";
+import jellyFragmentShader from "./shaders/hologram/fragment.glsl";
+
+console.log(jellyVertexShader);
+console.log(jellyFragmentShader);
+
+const jellyMaterial = new THREE.ShaderMaterial({
+	uniforms: {
+		uTime: 0,
+		uColor: new THREE.Color(0x00ff00),
+	},
+	transparent: true,
+	side: THREE.DoubleSide,
+	depthWrite: false,
+	vertexShader: jellyVertexShader,
+	fragmentShader: jellyFragmentShader,
+});
 
 export default function Experience() {
 	const testRef = useRef();
 	const jellyRef = useRef();
-	useFrame(
-		() => (testRef.current.rotation.x = testRef.current.rotation.y += 0.01)
-	);
+	// useFrame(
+	// 	() => (testRef.current.rotation.x = testRef.current.rotation.y += 0.01)
+	// );
 
 	useFrame((state, delta) => {
-		jellyRef.current.rotation.x = jellyRef.current.rotation.x += 0.01;
-		// (jellyRef.current.scale.y = 1 + 0.5 * Math.sin(delta * 2));
+		jellyRef.current.rotation.y += delta * 0.1;
+		// jellyRef.current.rotation.x += Math.sin(delta * 0.2);
 	});
 
-	const model = useGLTF("./model/jellyfish.glb");
+	// Traverse the scene to add the material to the jelly fish
+	const model = useGLTF("./model/jellyfish1.glb");
+	model.scene.traverse((child) => {
+		console.log(child.name);
+		if (child.name === "jelly") {
+			child.material = jellyMaterial;
+		}
+	});
 
 	return (
 		<>
 			<OrbitControls />
-
+			<directionalLight intensity={1} position={[2, -1, -1]} />
 			<ambientLight intensity={Math.PI / 2} />
-			<Center>
-				<primitive
-					object={model.scene}
-					// material={new THREE.MeshStandardMaterial({ color: "white" })}
-					ref={jellyRef}
-				/>
-			</Center>
-			<mesh ref={testRef}>
+			<primitive
+				object={model.scene}
+				position={[0, 1, 0]}
+				material={jellyMaterial}
+				ref={jellyRef}
+				scale={[0.3, 0.3, 0.3]}
+			/>
+			// Test mesh
+			{/* <mesh ref={testRef}>
 				<boxGeometry args={[1, 1]} position={[0, -1, 0]} />
 				<pointLight position={[1, 1, 1]} />
-				<meshStandardMaterial color='mediumpurple' />
-			</mesh>
+				<shaderMaterial
+					vertexShader={jellyVertexShader}
+					fragmentShader={jellyFragmentShader}
+				/>
+			</mesh> */}
 		</>
 	);
 }
--- src/shaders/hologram/fragment.glsl
+++ src/shaders/hologram/fragment.glsl
@@ -1,3 +1,22 @@
 void main() {
+
+  // Adjust the normal
+  
+  // Add the stripes 
+
+  // Add the Fresnel effect 
+  /** 
+  Formula for the fresnel effect 
+  1) Get the normal vector and the view direction
+  2) Calculate the dot product of the normal and the view direction
+  3) Add 1 to the dot product 
+  4) Reaise the result to a power
+  */
+
+  // Adjust the falloff 
+
+  // Add the holographic effect 
+
+  gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
   
 }
